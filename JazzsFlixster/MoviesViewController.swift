a91d4696aa59220c2e4b86b9a42d567a4f0db0ed
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-09-27 09:08:13 -0500

    Flix2: Superhero network request: similarity for collection view

--- JazzsFlixster/MoviesViewController.swift
+++ JazzsFlixster/MoviesViewController.swift
@@ -25,6 +25,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // TODO: Store the movies in a property to use elsewhere
         // TODO: Reload your table view data
         
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
