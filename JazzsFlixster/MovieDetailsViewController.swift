8f7497a0997bcfd7d296d5e19c7d4f7704d94c27
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-09-27 08:33:06 -0500

    Flix2: Details Screen

--- JazzsFlixster/MovieDetailsViewController.swift
+++ JazzsFlixster/MovieDetailsViewController.swift
@@ -6,16 +6,44 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
 
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     var movie: [String: Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+        
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        // ios doesn't provide a default way to download images
+        // but we can pull in third party libraries
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        // ios doesn't provide a default way to download images
+        // but we can pull in third party libraries
+        backdropView.af_setImage(withURL: backdropUrl!)
     }
     
 
