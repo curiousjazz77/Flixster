a8e181502201c658046540af04ad5fb90fd516ec
jomeke <10506008+curiousjazz77@users.noreply.github.com>
2020-09-27 10:08:40 -0500

    Flix2: Collection View Setup

---
+++ JazzsFlixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  JazzsFlixster
+//
+//  Created by Omeke, Jasmine on 9/27/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- JazzsFlixster/MovieGridViewController.swift
+++ JazzsFlixster/MovieGridViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     var movies = [[String: Any]]()
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -26,7 +32,7 @@ class MovieGridViewController: UIViewController {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             
               self.movies = dataDictionary["results"] as! [[String: Any]]
-            
+              self.collectionView.reloadData()
               // we'll have a collection view later
               print(self.movies)
 
@@ -37,6 +43,26 @@ class MovieGridViewController: UIViewController {
         task.resume()
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w780"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        // ios doesn't provide a default way to download images
+        // but we can pull in third party libraries
+        cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        return cell
+    }
     
 
     /*
